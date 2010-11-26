1c9f874287fc0fc85af47961dac5e5024704cc46
Alex Efros <powerman@cpan.org>
2010-11-26 12:44:09 +0000

    version 1.1.0

--- lib/Text/MiniTmpl.pm
+++ lib/Text/MiniTmpl.pm
@@ -4,7 +4,7 @@ use warnings;
 use strict;
 use Carp;
 
-use version; our $VERSION = qv('1.0.0');    # REMINDER: update Changes
+use version; our $VERSION = qv('1.1.0');    # REMINDER: update Changes
 
 # REMINDER: update dependencies in Makefile.PL
 use Perl6::Export::Attrs;
@@ -12,28 +12,25 @@ use JSON::XS qw( encode_json );
 use URI::Escape qw();
 use HTML::Entities qw();
 
-use constant UNSAFE_HTML => '&"\'<>' . join q{},map{chr}0..8,11,12,14..31,127;
-
+use constant UNSAFE_HTML=> '&"\'<>' . join q{},map{chr}0..8,11,12,14..31,127;
+use constant RAWPRE     => q{$}.__PACKAGE__.'::__ .= ';
+use constant RAWPOST    => q{};
+use constant UTF8PRE    => q{$}.__PACKAGE__.'::__utf8 = ';
+use constant UTF8POST   => 'utf8::encode($'.__PACKAGE__.'::__utf8);' .
+                           q{$}.__PACKAGE__.'::__ .= $'.__PACKAGE__.'::__utf8;';
 
 our $__;
+our $__utf8;
 our $TMPL_DIR = q{./};
 
 my %CACHE;
+my ($PRE, $POST) = (UTF8PRE, UTF8POST);
 
 
-# force newline after every bit of perl code and restore previous line number;
-# needed in this case:   <!--& #comment -->@~code()~@
-# _restoreline(1, "filename.pl") -- initialization
-# _restoreline("some\ntext\n")   -- calculate and return line number
-{ my ($line, $filename);
-  sub _restoreline {    ## no critic(RequireArgUnpacking)
-    if (@_ == 2) {
-        ($line, $filename) = @_;
-    } else {
-        $line += $_[0] =~ tr/\n//;
-    }
-    return("\n#line $line \"$filename\"\n");
-  }
+sub raw :Export {
+    my ($is_raw) = @_;
+    ($PRE, $POST) = $is_raw ? (RAWPRE, RAWPOST) : (UTF8PRE, UTF8POST);
+    return;
 }
 
 sub render :Export {
@@ -50,46 +47,41 @@ sub tmpl2code :Export {
     1 while $path =~ s{(\A|/) (?!\.\.?/) [^/]+/\.\./}{$1}xms; ## no critic(ProhibitPostfixControls)
     my $dir = $path;
     $dir =~ s{/[^/]*\z}{/}xms;
+    my $line = 1;
     my $e
         = 'package '.scalar(caller).'; use warnings; use strict;'
         . 'sub {'
         . 'local $'.__PACKAGE__.'::__ = q{};'
         . 'local $'.__PACKAGE__."::TMPL_DIR = \"\Q$dir\E\";"
         . 'local %_ = @_;'
-        . _restoreline(1, $path)
+        . "\n#line $line \"$path\"\n"
         ;
     open my $fh, '<', $path or croak "open: $!";
     my $s = do { local $/ = undef; <$fh> };
     close $fh or croak "close: $!";
     while ( 1 ) {
-        ($s=~/\G<!--&(.*?)-->/xmsgc)    && do {
-            $e .= "$1;";
-            $e .= _restoreline($1);
-            }
-     || ($s=~/\G&~(.*?)~&/xmsgc)        && do {
-            $e .= "$1;";
-            $e .= _restoreline($1);
-            }
-     || ($s=~/\G@~(.*?)~@/xmsgc)        && do {
-            $e .= q{$}.__PACKAGE__.'::__ .= '.__PACKAGE__."::_utf8_encode(HTML::Entities::encode_entities(''.(do { $1; }), ".__PACKAGE__.'::UNSAFE_HTML)) ;';
-            $e .= _restoreline($1);
-            }
-     || ($s=~/\G\#~(.*?)~\#/xmsgc)      && do {
-            $e .= q{$}.__PACKAGE__.'::__ .= '.__PACKAGE__."::_utf8_encode(do { $1; }) ;";
-            $e .= _restoreline($1);
-            }
-     || ($s=~/\G\^~(.*?)~\^/xmsgc)      && do {
-            $e .= q{$}.__PACKAGE__.'::__ .= '.__PACKAGE__."::_utf8_encode(URI::Escape::uri_escape_utf8(''.(do { $1; }))) ;";
-            $e .= _restoreline($1);
-            }
-     || ($s=~/\G(.*?)(?=<!--&|&~|@~|\#~|\^~|\n|$)(\n?)/xmsgc) && do {
-            $e .= q{$}.__PACKAGE__."::__ .= \"\Q$1\E" . ($2 ? '\n' : q{}) . "\";$2";
-            _restoreline($1.$2);
-            }
-     || last;
+        $e .=
+            $s=~/\G<!--& ( (?>[^-]*) .*? ) -->/xmsgc
+                ? "$1;"
+          : $s=~/\G&~    ( (?>[^~]*) .*? ) ~&/xmsgc
+                ? "$1;"
+          : $s=~/\G@~    ( (?>[^~]*) .*? ) ~@/xmsgc
+                ? $PRE."HTML::Entities::encode_entities(''.(do { $1; }), ".__PACKAGE__.'::UNSAFE_HTML);'.$POST
+          : $s=~/\G\#~   ( (?>[^~]*) .*? ) ~\#/xmsgc
+                ? $PRE."do { $1; };".$POST
+          : $s=~/\G\^~   ( (?>[^~]*) .*? ) ~\^/xmsgc
+                ? $PRE."URI::Escape::uri_escape_utf8(''.(do { $1; }));".$POST
+          : $s=~/\G      ( (?>[^<&@\#^]*) .*? ) (?=<!--&|&~|@~|\#~|\^~|\z)/xmsgc
+                ? q{$}.__PACKAGE__."::__ .= \"\Q$1\E\";"
+          : last;
+        $line += $1 =~ tr/\n//;
+        $e .= "\n#line $line \"$path\"\n";
     }
     $e .= '; return \$'.__PACKAGE__.'::__ }';
-    my $code = eval $e; ## no critic(ProhibitStringyEval)
+    # do instead of eval to have better diagnostics and support source filters
+    open my $fhe, '<', \$e or croak "open: $!"; ## no critic(RequireBriefOpen)
+    local @INC = ( sub {shift @INC; $fhe}, @INC );
+    my $code = do '[eval]';
     croak $@ if $@;
     return $code;
 }
@@ -105,13 +97,6 @@ sub encode_js_data :Export {
     my ($s) = @_;
     $s = encode_json($s);
     $s =~ s{</script}{<\\/script}xmsg;
-    return $s;
-}
-
-# faster than Encode::encode() and doesn't require temp var before append to $__
-sub _utf8_encode {  ## no critic(ProhibitUnusedPrivateSubroutines)
-    my ($s) = @_;
-    utf8::encode($s);
     return $s;
 }
 
@@ -178,6 +163,22 @@ number.
 Files with templates should be in UTF8. Parameters for templates should be
 perl Unicode scalars. Rendered template (returned by render() or by
 function returned by tmpl2code()) will be in UTF8.
+
+You can disable it using raw(1) (see below) to get more speed.
+
+=head2 Source Filters support
+
+Probably not all filters will work inside templates - keep in mind filter
+will see autogenerated (by tmpl2code()) perl function's code instead of
+plain template text. See `perldoc perlfilter` for more details.
+
+Example:
+
+    &~ use Filter::CommaEquals; ~&
+    &~ @{ $_{users} } ,= 'GHOST' ~&
+    &~ for (@{ $_{users} }) { ~&
+    <p>Hello, @~ $_ ~@!</p>
+    &~ } ~&
 
 =head2 Template syntax
 
@@ -289,6 +290,21 @@ Example:
 Return CODEREF to that function.
 
 
+=item raw( $is_raw )
+
+If $is_raw TRUE disable Unicode support.
+To enable Unicode again call raw() with $is_raw FALSE.
+
+B<Disabling Unicode support will speedup this module in about 1.5 times!>
+
+When Unicode support disabled your parameters used to render template will
+be used in template AS IS, without attempt to encode them to UTF8.
+This mean you shouldn't use perl Unicode scalars in these parameters anymore.
+
+This affect only templates processed by tmpl2code() after calling raw()
+(beware caching effect of render()).
+
+
 =item encode_js( $scalar )
 
 Encode $scalar (string or number) for inserting into JavaScript code
@@ -318,6 +334,58 @@ Example:
     </script>
 
 Return encoded string.
+
+
+=back
+
+
+=head1 SPEED AND SIZE
+
+This module implemented under 100 lines (about 3KB) of pure Perl code.
+And while code is terse enough, I believe it still simple and clean.
+
+While this is pure-perl module with many enough features, it's still very
+fast - you can do your own benchmarking using cool L<Template::Benchmark>
+module, here is results from my system:
+
+=over
+
+=item instance_reuse
+
+This test simulate standard FastCGI which read template from HDD and
+compile it only once (when this template requested first time), and for
+next requests it just render it using cached anon subroutine.
+
+            Rate   XS?
+    TT        29/s  -  Template::Toolkit (2.22)
+    TT_X      95/s  Y  Template::Toolkit (2.22) with Stash::XS
+    HM       704/s  -  HTML::Mason (1.45)
+    TeCS     738/s  Y  Text::ClearSilver (0.10.5.4)
+    TeMT    1131/s  -  Text::MicroTemplate (0.18)
+    TeMMHM  1173/s  -  Text::MicroMason (2.12) using Text::MicroMason::HTMLMason
+  * TMTU    1357/s  -  Text::MiniTmpl (1.1.0) with enabled Unicode
+    MoTe    1629/s  -  Mojo::Template ()
+  * TMT     2054/s  -  Text::MiniTmpl (1.1.0)
+    TeClev  5966/s  Y  Text::Clevery (0.0004) in XS mode
+    TeXs    6761/s  Y  Text::Xslate (0.2015)
+
+=item uncached_disk
+
+This test simulate standard CGI which read template from HDD, compile and
+render it on each run - no caches used at all (except HDD files caching by OS).
+
+            Rate   XS?
+    TeXs     1.4/s  Y  Text::Xslate (0.2015)
+    TeClev   1.5/s  Y  Text::Clevery (0.0004) in XS mode
+    HM      12.6/s  -  HTML::Mason (1.45)
+    MoTe    21.0/s  -  Mojo::Template ()
+    TeMT    32.1/s  -  Text::MicroTemplate (0.18)
+    TeMMHM  36.2/s  -  Text::MicroMason (2.12) using Text::MicroMason::HTMLMason
+  * TMTU    54.1/s  -  Text::MiniTmpl (1.1.0) with enabled Unicode
+  * TMT     67.9/s  -  Text::MiniTmpl (1.1.0)
+    TeTmpl   448/s  Y  Text::Tmpl (0.33)
+    TeCS     725/s  Y  Text::ClearSilver (0.10.5.4)
+    HTP     1422/s  Y  HTML::Template::Pro (0.9504)
 
 
 =back
